bf374cd8e295d57e9d0140700ed18c90a40031eb
Juan Manuel <justin4949ch@gmail.com>
2023-03-03 17:48:52 -0500

    insertacion del local storage

--- taller de tweets/src/App.jsx
+++ taller de tweets/src/App.jsx
@@ -1,32 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react'
-export const EjemploEvento = () => {
-  const [text, setText] = useState("");
-  const [greatting, setGreatting] = useState('aqui veras tu tweet');
-  const [charCount, setCharCount] = useState(0);
-  const handleEvent = () => { setGreatting(text) }
+import React from 'react'
+import { EjemploEvento } from './home/Home'
 
-
-  useEffect(() => {
-  const charatersmax=255;
-  setCharCount(charatersmax-text.length);
-  }, [text]);
-  return (
+function App(){
+  return(
     <>
-      <h1>Generador de tweets</h1>
-      <div>
-        <h2>publica tus tweets</h2>
-        <hr />
-        <div>
-          <textarea placeholder='maximo de caracteres 255' onChange={(e) => setText(e.target.value)} id='txt-into' className='Txt-area' type="text" />
-        </div>
-        <p >te quedan {charCount} </p>
-        <button onClick={handleEvent} className='btn-greatting'>publicar</button>
-        <button onClick={handleEvent} className='btn-greatting'>archivar</button>
-        <button onClick={handleEvent} className='btn-greatting'>mostrar archivos</button>
-      </div>
-      <h3>{greatting}</h3>
-
-    {/* hola mundo */}
+    <EjemploEvento />
     </>
   )
 }
+export default App;
---
+++ taller de tweets/src/home/Home.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+export const EjemploEvento = () => {
+    const [text, setText] = useState('');
+    // const [text, setText] = useState(window.localStorage.getItem('text'));
+    const [greatting, setGreatting] = useState('aqui veras tu tweet');
+    const [charCount, setCharCount] = useState(0);
+    const [valor, setValor] = useState();
+    const handleEvent = () => { setGreatting(text) }
+    const returnLocalStorage = () => { setGreatting(window.localStorage.getItem('text')) }
+    const clearLocalStorage = () => { localStorage.clear() }
+
+
+
+    useEffect(() => {
+        const charatersmax = 255;
+        setCharCount(charatersmax - text.length)
+        if (charCount === 1) {
+            alert("usted a llegado a su limite")
+            setValor(255)
+        }
+    }, [text]);
+
+    const setLocalStorage = value => {
+        try {
+            setText(value)
+            window.localStorage.setItem("text", [value])
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    return (
+        <>
+            <h1>Generador de tweets</h1>
+            <div>
+                <h2>publica tus tweets</h2>
+                <hr />
+                <div>
+                    <textarea placeholder='maximo de caracteres 255' onChange={(e) => setLocalStorage(e.target.value)} id='txt-into' className='Txt-area' value={text} type="text" maxLength={valor} />
+                </div>
+                <p >te quedan {charCount} </p>
+                <button onClick={handleEvent} className='btn-greatting'>publicar tweet actual</button>
+                <button onClick={clearLocalStorage} className='btn-greatting'>borrar tweets guardados</button>
+                <button onClick={returnLocalStorage} className='btn-greatting'>mostrar ultimo tweet</button>
+            </div>
+            <h3>{greatting}</h3>
+
+            {/* hola mundo */}
+        </>
+    )
+}
